e5e88c8438228bc639fc6b54a6e5e1393275fb22
Aidan Horton <aidan.horton@virginmedia.com>
2021-05-07 23:19:08 +0100

    Re-add files with gitignore settings

---
+++ InertiaChess/InertiaChess.Presentation/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Ioc;
+using Prism.Unity;
+using System.Windows;
+
+namespace InertiaChess.Presentation
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : PrismApplication
+    {
+        protected override Window CreateShell()
+        {
+            return Container.Resolve<Shell>();
+        }
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+            // Register types here.
+        }
+    }
+}
---
+++ InertiaChess/InertiaChess.Presentation/Views/Shell.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace InertiaChess.Presentation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class Shell : Window
+    {
+        public Shell()
+        {
+            InitializeComponent();
+        }
+    }
+}

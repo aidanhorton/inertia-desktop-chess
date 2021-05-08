4ef41259b000a71727358cd06c093262a325b962
Aidan Horton <aidan.horton@virginmedia.com>
2021-05-08 20:13:40 +0100

    Add basic toolbar dropdown buttons

---
+++ InertiaChess/InertiaChess.Presentation/Behaviors/DropDownButtonBehavior.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.Xaml.Behaviors;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+
+namespace InertiaChess.Presentation.Behaviors
+{
+    public class DropDownButtonBehavior : Behavior<ToggleButton>
+    {
+        private ToggleButton button;
+        private bool isContextMenuOpen;
+
+        protected override void OnAttached()
+        {
+            base.OnAttached();
+            AssociatedObject.AddHandler(ToggleButton.ClickEvent, new RoutedEventHandler(AssociatedObject_Click), true);
+        }
+
+        void AssociatedObject_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            this.button = sender as ToggleButton;
+            if (this.button != null && this.button.ContextMenu != null)
+            {
+                if (!isContextMenuOpen)
+                {
+                    // Add handler to detect when the ContextMenu closes
+                    this.button.ContextMenu.AddHandler(ContextMenu.ClosedEvent, new RoutedEventHandler(ContextMenu_Closed), true);
+                    // If there is a drop-down assigned to this button, then position and display it 
+                    this.button.ContextMenu.PlacementTarget = this.button;
+                    this.button.ContextMenu.Placement = PlacementMode.Bottom;
+                    this.button.ContextMenu.IsOpen = true;
+                    isContextMenuOpen = true;
+                }
+            }
+        }
+
+        protected override void OnDetaching()
+        {
+            base.OnDetaching();
+            AssociatedObject.RemoveHandler(ToggleButton.ClickEvent, new RoutedEventHandler(AssociatedObject_Click));
+        }
+
+        private void ContextMenu_Closed(object sender, RoutedEventArgs e)
+        {
+            if (this.button != null)
+            {
+                this.button.IsChecked = false;
+            }
+
+            isContextMenuOpen = false;
+            var contextMenu = sender as ContextMenu;
+            if (contextMenu != null)
+            {
+                contextMenu.RemoveHandler(ContextMenu.ClosedEvent, new RoutedEventHandler(ContextMenu_Closed));
+            }
+        }
+    }
+}
